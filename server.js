c35af2773c594740aef0ca6d4548ccd5fbedd3ca
Pablo Arak <pablo.arak.vm@gmail.com>
2022-03-11 12:58:24 -0300

    Added some configs and first routes

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
-});
-
-// Listen to the App Engine-specified port, or 8080 otherwise
+const morgan = require('morgan');
+const main = require('./routes/main');
 const PORT = process.env.PORT || 8080;
+
+// Body parser
+app.use(express.json());
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+// Routes
+app.use(main);
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
